d0fbd4303e0e3a6cca51fdcd1ed72afe8e9c609c
costadefelipe@gmail.com <costadefelipe@gmail.com>
2020-10-09 09:42:02 -0300

    gobootcamp array

---
+++ cmd/38_array.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+  a := [...]string{"Hello, World!"}
+  fmt.Println("%q", a)
+}
---
+++ cmd/39_array.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+  a := [2]string{"Hello", "World"}
+  fmt.Println(a)
+  // [Hello World]
+  fmt.Printf("%s\n", a)
+  // [Hello World]
+  fmt.Printf("%q\n", a)
+  // ["Hello", "World"]
+}
