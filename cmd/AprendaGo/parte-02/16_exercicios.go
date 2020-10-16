1f6e0534d05a816859c6b0f12e11e53b1fbc202e
costadefelipe@gmail.com <costadefelipe@gmail.com>
2020-10-16 13:39:19 -0300

    exercicios parte 2

---
+++ cmd/AprendaGo/parte-02/16_exercicios.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+var a int = 100
+
+func main(){
+    fmt.Printf("%b - %#x - %v", a, a, a)
+}
---
+++ cmd/AprendaGo/parte-02/17_exercicios.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main()  {
+    x := (10 > 5)
+    y := (9 == 9)
+    t := (100 < 200)
+    z := (2 != 2)
+    yui := (10 <= 10)
+    zui := (10 >= 100)
+
+    fmt.Println(x)
+    fmt.Println(y)
+    fmt.Println(t)
+    fmt.Println(z)
+    fmt.Println(yui)
+    fmt.Println(zui)
+}
