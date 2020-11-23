74e0ee0042b31483e65f6453f0d530e87b46cbc1
costadefelipe@gmail.com <costadefelipe@gmail.com>
2020-11-23 08:53:38 -0300

    revisando

--- cmd/AprendaGo/parte-01/03_variaveis.go
+++ cmd/AprendaGo/parte-01/03_variaveis.go
@@ -6,8 +6,12 @@ func main() {
   a := 1
   b := "hello, World!"
   c := true
+
+  c = false
   
-  fmt.Println(a, b, c)
+  erros, errors := fmt.Println(a, b, c)
   fmt.Printf("a:= %v a:= %T\n", a, a)
   fmt.Printf("b:= %v b:= %T", b, b)
+
+  fmt.Println("\n", erros, errors)
 }
